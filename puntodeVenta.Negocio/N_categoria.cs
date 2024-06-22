1befb4cb87e86cb251e19ff4a9f28e3f9a9c6c02
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-06-22 10:12:53 -0600

    Se renombra la clase N-categorias a N_categorias

---
+++ puntodeVenta.Negocio/N_categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using puntoVenta.Entidad;
+using puntoVenta.Datos;
+
+namespace puntodeVenta.Negocio
+{
+    public class N_categoria
+    {
+        public static DataTable listado_ca(string cTexto) { 
+
+            D_categoria datos = new D_categoria();
+
+            return datos.listado_categoria(cTexto);
+        }
+    }
+}
